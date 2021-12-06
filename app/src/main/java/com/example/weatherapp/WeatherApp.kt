1b81b7c2e92d05cbe267dd2f76aa3fe8d49ef7c1
batuhanduvarci <batuhanduvarci@gmail.com>
2021-12-06 19:43:39 +0300

    Add hilt android application

---
+++ app/src/main/java/com/example/weatherapp/WeatherApp.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Batuhan Duvarci on 6.12.2021.
+ */
+@HiltAndroidApp
+class WeatherApp : Application() {
+}
