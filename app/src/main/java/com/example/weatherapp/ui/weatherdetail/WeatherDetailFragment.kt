6536f6d2885757b5e7200f5ccf66466b05250115
Batuhan DUVARCI <batuhan.duvarci@veripark.com>
2021-12-07 18:36:32 +0300

    Add service integration

--- app/src/main/java/com/example/weatherapp/ui/weatherdetail/WeatherDetailFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/weatherdetail/WeatherDetailFragment.kt
@@ -2,10 +2,16 @@ package com.example.weatherapp.ui.weatherdetail
 
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenStarted
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentWeatherDetailBinding
 import com.example.weatherapp.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class WeatherDetailFragment : BaseFragment<FragmentWeatherDetailBinding, WeatherDetailViewModel>(R.layout.fragment_weather_detail) {
 
     override val viewModel: WeatherDetailViewModel by viewModels()
@@ -21,6 +27,12 @@ class WeatherDetailFragment : BaseFragment<FragmentWeatherDetailBinding, Weather
     }
 
     override fun startCoroutine() {
-        //TODO("Not yet implemented")
+        with(viewLifecycleOwner) {
+            lifecycleScope.launch(Dispatchers.IO) {
+                whenStarted {
+                    viewModel.getWeatherData(38.423733, 27.142826, "8ddadecc7ae4f56fee73b2b405a63659")
+                }
+            }
+        }
     }
 }
