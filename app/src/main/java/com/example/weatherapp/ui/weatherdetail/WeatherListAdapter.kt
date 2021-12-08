4ad2c0ed7923336d95eb28ced6d1b4354ab01cfb
Batuhan DUVARCI <batuhan.duvarci@veripark.com>
2021-12-08 12:36:19 +0300

    Add resources access on weather view holder

--- app/src/main/java/com/example/weatherapp/ui/weatherdetail/WeatherListAdapter.kt
+++ app/src/main/java/com/example/weatherapp/ui/weatherdetail/WeatherListAdapter.kt
@@ -1,20 +1,22 @@
 package com.example.weatherapp.ui.weatherdetail
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.example.weatherapp.R
 import com.example.weatherapp.domain.models.DailyWeatherItemModel
+import javax.inject.Inject
 
 /**
  * Created by Batuhan Duvarci on 7.12.2021.
  */
-class WeatherListAdapter : ListAdapter<DailyWeatherItemModel, WeatherViewHolder>(WeatherListItemDiffCallback()) {
+class WeatherListAdapter(private val resources: Resources) : ListAdapter<DailyWeatherItemModel, WeatherViewHolder>(WeatherListItemDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
-        return WeatherViewHolder(view)
+        return WeatherViewHolder(view, resources)
     }
 
     override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
--- app/src/main/java/com/example/weatherapp/ui/weatherdetail/WeatherViewHolder.kt
+++ app/src/main/java/com/example/weatherapp/ui/weatherdetail/WeatherViewHolder.kt
@@ -1,23 +1,25 @@
 package com.example.weatherapp.ui.weatherdetail
 
+import android.content.res.Resources
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.weatherapp.R
 import com.example.weatherapp.databinding.ItemWeatherBinding
 import com.example.weatherapp.domain.models.DailyWeatherItemModel
 
 /**
  * Created by Batuhan Duvarci on 7.12.2021.
  */
-class WeatherViewHolder(private val view : View) : RecyclerView.ViewHolder(view) {
+class WeatherViewHolder(private val view : View, private val resources: Resources) : RecyclerView.ViewHolder(view) {
     fun bind(weather: DailyWeatherItemModel) {
         val binding = ItemWeatherBinding.bind(view)
         with(binding){
             dayTextView.text = weather.date
             Glide.with(view).load(weather.dailyWeather.last().icon).into(weatherImageView)
             weather.temp?.let {
-                degreeHighestTextView.text = it.max.toString()
-                degreeLowestTextView.text = it.min.toString()
+                degreeHighestTextView.text = String.format(resources.getString(R.string.fragment_weather_detail_degree_text), it.max)
+                degreeLowestTextView.text = String.format(resources.getString(R.string.fragment_weather_detail_degree_text), it.min)
             }
         }
     }
