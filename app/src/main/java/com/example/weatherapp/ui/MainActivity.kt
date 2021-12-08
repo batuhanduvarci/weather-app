06619d8cfc1f6689d575f230a7eaa1bf96837574
Batuhan DUVARCI <batuhan.duvarci@veripark.com>
2021-12-08 12:34:27 +0300

    Add location permission on activity start

--- app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weatherapp.databinding.ActivityMainBinding
+import com.example.weatherapp.gps.LocationUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        LocationUtils.requestLocationPermission(this)
     }
 }
